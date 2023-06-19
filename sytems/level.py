6602f2c731e22c2a96627af8771313b789dfee9f
-Lunat1c- <arturgrabowski100@onet.pl>
2023-06-19 20:53:11 +0200

    Basic Player implementation

--- sytems/level.py
+++ sytems/level.py
@@ -1,4 +1,6 @@
 import pygame
+
+from models.player import Player
 from models.tile import Tile
 from settings import TILE_SIZE
 
@@ -7,20 +9,30 @@ class Level:
     def __init__(self, level_data, surface):
         self.display_surface = surface
         self.tiles = pygame.sprite.Group()
+        self.player = pygame.sprite.GroupSingle()
         self.setup_level(level_data)
 
         self.world_shift = 0
 
     def setup_level(self, layout):
         self.tiles = pygame.sprite.Group()
+        self.player = pygame.sprite.GroupSingle()
+
         for row_index, row in enumerate(layout):
             for col_index, cell in enumerate(row):
+                x = col_index * TILE_SIZE
+                y = row_index * TILE_SIZE
+
                 if cell == 'X':
-                    x = col_index * TILE_SIZE
-                    y = row_index * TILE_SIZE
                     tile = Tile((x, y), TILE_SIZE)
                     self.tiles.add(tile)
+                if cell == 'P':
+                    player_sprite = Player((x, y))
+                    self.player.add(player_sprite)
 
     def run(self):
         self.tiles.update(self.world_shift)
         self.tiles.draw(self.display_surface)
+
+        self.player.update()
+        self.player.draw(self.display_surface)
