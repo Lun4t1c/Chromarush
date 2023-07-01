86277e6bec6c3d11734a03071ee3347a39691daa
-Lunat1c- <arturgrabowski100@onet.pl>
2023-07-01 14:40:57 +0200

    Added graphics and implemented basic animation

--- models/player.py
+++ models/player.py
@@ -1,11 +1,18 @@
 import pygame
 
+from utils.helpers import import_folder
+
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos):
         super().__init__()
-        self.image = pygame.Surface((32, 64))
-        self.image.fill('red')
+        self.animations = None
+        self.import_character_assets()
+        print(self.animations)
+
+        self.frame_index = 0
+        self.animation_speed = 0.15
+        self.image = self.animations['idle'][self.frame_index]
         self.rect = self.image.get_rect(topleft=pos)
 
         # player movement
@@ -13,6 +20,28 @@ class Player(pygame.sprite.Sprite):
         self.direction = pygame.math.Vector2(0, 0)
         self.gravity = 0.8
         self.jump_speed = -16
+
+    def import_character_assets(self):
+        character_path = 'assets/graphics/character/'
+        self.animations = {
+            'idle': [],
+            'run': [],
+            'jump': [],
+            'fall': []
+        }
+
+        for animation in self.animations.keys():
+            full_path = character_path + animation
+            self.animations[animation] = import_folder(full_path)
+
+    def animate(self):
+        animation = self.animations['idle']
+
+        self.frame_index += self.animation_speed
+        if self.frame_index >= len(animation):
+            self.frame_index = 0
+
+        self.image = animation[int(self.frame_index)]
 
     def get_input(self):
         keys = pygame.key.get_pressed()
@@ -36,3 +65,4 @@ class Player(pygame.sprite.Sprite):
 
     def update(self):
         self.get_input()
+        self.animate()
